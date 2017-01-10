1e035dd3d8bfcb8420f9b4677178fd85d552e80a
nachostein <4712IUpfa>
2017-01-10 16:51:34 -0300

    v5
    deploy aaun

--- blog.py
+++ blog.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!This is the Opsgadget Blog from Nacho Stein Lumiserv Homework v5 deploy'
+    return 'Hello, World!This is the Opsgadget Blog from Nacho Stein Lumiserv Homework v5 deploy v2'
